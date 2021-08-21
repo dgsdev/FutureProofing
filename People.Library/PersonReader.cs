3cc870b318d16b796a887ed9f8b8752d7095f8d9
Douglas Pinheiro <dgspramos@gmail.com>
2021-08-20 23:33:45 -0300

    Classe Person

--- People.Library/PersonReader.cs
+++ People.Library/PersonReader.cs
@@ -1,17 +1,43 @@
-﻿using System;
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
 
 namespace People.Library
 {
     public class PersonReader
     {
-        public PersonReader[] GetPeople()
-        {
 
+        #region Service initializers
+        WebClient client = new WebClient();
+        string baseUri = "http://localhost:9874";
+        JsonSerializerOptions options =
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        #endregion
+
+        public Person[] GetPeople()
+        {
+            string address = $"{baseUri}/people";
+            string reply = client.DownloadString(address);
+            var result = JsonSerializer.Deserialize<List<Person>>(reply, options);
+            if (result is null)
+            {
+                result = new List<Person>();
+            }
+            return result.ToArray();
         }
 
-        public PersonReader GetPerson(int id)
+        public Person GetPerson(int id)
         {
-
+            var address = $"{baseUri}/people/{id}";
+            string reply = client.DownloadString(address);
+            var result = JsonSerializer.Deserialize<Person>(reply, options);
+            if (result is null)
+            {
+                result = new Person();
+            }
+            return result;
         }
     }
 }
